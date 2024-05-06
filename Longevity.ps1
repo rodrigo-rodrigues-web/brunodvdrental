eb3caea9bf27c12c2899aa5cc5e4190d235a05d9
Rodrigo <rmirandarodrigues@outlook.com>
2024-05-06 16:11:41 -0300

    Initial commit

---
+++ Longevity.ps1
@@ -0,0 +1,14 @@
+﻿# Tip: open it in VsCode
+# Tip2: use the PS script CollectServerInfo.ps1 to collect info on the server.
+# deletes the results.json file then creates a fresh file. Every interation the result is appended to it.
+cd 'C:\bruno\dvdrental\'
+$json = 'results.json'
+if(test-path results.json) {
+    Remove-Item -Path $json 
+
+}
+New-Item -Name $json -ItemType File
+for ($i = 0; $i -lt 5; $i++) {
+    $res = bru run --env dvdrental
+    Add-Content -Path $json -Value $res
+}
