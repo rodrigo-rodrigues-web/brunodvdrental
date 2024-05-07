4fe051c287805fe9133897383d640d665bce5f56
Rodrigo <rmirandarodrigues@outlook.com>
2024-05-07 10:31:55 -0300

    timestamp added

--- Longevity.ps1
+++ Longevity.ps1
@@ -1,6 +1,7 @@
 ﻿# Tip: open it in VsCode
 # Tip2: use the PS script CollectServerInfo.ps1 to collect info on the server.
 # deletes the results.json file then creates a fresh file. Every interation the result is appended to it.
+$start = "Start Time: " + (Get-Date)
 cd 'C:\bruno\dvdrental\'
 $json = 'results.json'
 if(test-path results.json) {
@@ -8,7 +9,12 @@ if(test-path results.json) {
 
 }
 New-Item -Name $json -ItemType File
-for ($i = 0; $i -lt 5; $i++) {
+for ($i = 0; $i -lt 1; $i++) {
     $res = bru run --env dvdrental
     Add-Content -Path $json -Value $res
+    $res
 }
+$finish = "Finish Time: " + (Get-Date)
+
+$start
+$finish
